6a61e7ea614b632bbb33dc8a67d3eff8571902bc
DawalaGH <dawalaamidudar@gmail.com>
2025-01-05 16:55:27 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("WELCOME TO OUR SERVICES")
+a=10
+b="Joe"
+print(a)
+print(b)
+#input statement
+first_name=input("Enter first name:")
+surname=input("Enter surname:")
+age=input("Enter your age:")
+registration_number=int(input("Enter your registration number:"))
+grade=input("Enter your grade:")
