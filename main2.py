c2d27d7bb80eda2b960dcfa54e90004169ddf765
DawalaGH <dawalaamidudar@gmail.com>
2025-01-12 22:21:08 +0000

    update

---
+++ main2.py
@@ -0,0 +1,6 @@
+name=input("enter name:")
+month=input("enter birthday month:")
+day=input("enter birthday day:")
+year=input("enter birthday year:")
+
+print("Hello",name,"your birthday is on ",month,"/",day,"/",year)
